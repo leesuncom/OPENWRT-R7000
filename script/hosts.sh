d09bf8a45de7beca9492a0765d0300770f2eb81d
leesuncom <1925600159@qq.com>
2025-08-25 14:56:33 +0800

    Delete hosts.sh

--- script/hosts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# 删除
-sudo sed -i '/# ING Hosts Start/,/# ING Hosts End/d' common/etc/hosts
-# 添加
-curl -s -k -L https://ghfast.top/https://raw.githubusercontent.com/shidahuilang/hosts/main/hosts | sudo tee -a common/etc/hosts
