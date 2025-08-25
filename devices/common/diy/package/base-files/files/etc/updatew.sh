6386f0425c9a10ae2e16bb788ce388edfd94acc9
leesuncom <1925600159@qq.com>
2025-08-25 22:01:23 +0800

    Update updatew.sh

--- devices/common/diy/package/base-files/files/etc/updatew.sh
+++ devices/common/diy/package/base-files/files/etc/updatew.sh
@@ -15,22 +15,22 @@ echo "spki_OpenDNS(Cisco): $(echo | openssl s_client -connect '208.67.222.222:85
 curl -sS https://www.cloudflare.com/ips-v4/ > /etc/smartdns/cloudflare-ipv4.txt
 
 # Update blacklist-ip
-curl -sS https://raw.githubusercontent.com/leesuncom/NetGearR7000/refs/heads/main/common/etc/smartdns/blacklist-ip.conf > /etc/smartdns/blacklist-ip.conf
+curl -sS https://ghfast.top/https://raw.githubusercontent.com/leesuncom/NetGearR7000/refs/heads/main/common/etc/smartdns/blacklist-ip.conf > /etc/smartdns/blacklist-ip.conf
 
 # Update hosts
 sudo sed -i '/# ING Hosts Start/,/# ING Hosts End/d' /etc/hosts
 curl -s -k -L https://ghfast.top/https://raw.githubusercontent.com/shidahuilang/hosts/main/hosts | sudo tee -a /etc/hosts
 
 # Update China IPV4 List
-curl -sS https://raw.githubusercontent.com/leesuncom/NetGearR7000/main/common/etc/smartdns/domain-set/domains.china.smartdns.conf -o /tmp/domains.china.smartdns.conf
+curl -sS https://ghfast.top/https://raw.githubusercontent.com/leesuncom/NetGearR7000/main/common/etc/smartdns/domain-set/domains.china.smartdns.conf -o /tmp/domains.china.smartdns.conf
 cp /tmp/domains.china.smartdns.conf /etc/smartdns/domain-set/domains.china.smartdns.conf
 
 # Update GFW List
-curl -sS https://raw.githubusercontent.com/leesuncom/NetGearR7000/main/common/etc/smartdns/domain-set/proxy-domain-list.conf -o /tmp/proxy-domain-list.conf
+curl -sS https://ghfast.top/https://raw.githubusercontent.com/leesuncom/NetGearR7000/main/common/etc/smartdns/domain-set/proxy-domain-list.conf -o /tmp/proxy-domain-list.conf
 cp /tmp/proxy-domain-list.conf /etc/smartdns/domain-set/proxy-domain-list.conf
 
 # Update address.conf
-# curl -sS https://raw.githubusercontent.com/Cats-Team/AdRules/main/smart-dns.conf -o /tmp/address.conf
+# curl -sS https://ghfast.top/https://raw.githubusercontent.com/Cats-Team/AdRules/main/smart-dns.conf -o /tmp/address.conf
 # cp /tmp/address.conf /etc/smartdns/address.conf
 
 # onf-file /etc/smartdns/anti-ad-smartdns.conf
