a04332326ce2ae9cc0afabb5b174e08cf66efa71
leesuncom <1925600159@qq.com>
2025-08-25 22:59:22 +0800

    Update updatew.sh

--- devices/common/diy/package/base-files/files/etc/updatew.sh
+++ devices/common/diy/package/base-files/files/etc/updatew.sh
@@ -1,43 +1,42 @@
-#!/bin/bash
+#!/bin/sh
 
-# Update DNS SPKI
-echo "spki_cloudflare: $(echo | openssl s_client -connect '1.0.0.1:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" > /etc/smartdns/spki
-# 8.8.8.8 SPKI 
-echo "spki_google: $(echo | openssl s_client -connect '8.8.8.8:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> /etc/smartdns/spki
-# DNSPod SPKI
-echo "spki_DNSPod: $(echo | openssl s_client -connect '120.53.53.53:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> /etc/smartdns/spki
-# SB DNS SPKI 
-echo "spki_SB: $(echo | openssl s_client -connect '185.222.222.222:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> /etc/smartdns/spki
-# OpenDNS(Cisco) SPKI
-echo "spki_OpenDNS(Cisco): $(echo | openssl s_client -connect '208.67.222.222:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> /etc/smartdns/spki
-
-# Update cloudflare-ipv4
-curl -sS https://www.cloudflare.com/ips-v4/ > /etc/smartdns/cloudflare-ipv4.txt
-
-# Update blacklist-ip
-curl -sS https://ghfast.top/https://raw.githubusercontent.com/leesuncom/NetGearR7000/refs/heads/main/common/etc/smartdns/blacklist-ip.conf > /etc/smartdns/blacklist-ip.conf
-
-# Update hosts
 sudo sed -i '/# ING Hosts Start/,/# ING Hosts End/d' /etc/hosts
 curl -s -k -L https://ghfast.top/https://raw.githubusercontent.com/shidahuilang/hosts/main/hosts | sudo tee -a /etc/hosts
 
-# Update China IPV4 List
+# 更新IP黑名单
+rm -f /tmp/blacklist-ip.conf
+curl -sS https://ghfast.top/https://raw.githubusercontent.com/leesuncom/NetGearR7000/main/common/etc/smartdns/blacklist-ip.conf -o /tmp/blacklist-ip.conf
+cp /tmp/blacklist-ip.conf /etc/smartdns/blacklist-ip.conf
+
+# 更新Cloudflare IPv4列表
+rm -f /tmp/cloudflare-ipv4.txt
+curl -sS https://www.cloudflare.com/ips-v4/ -o /tmp/cloudflare-ipv4.txt
+cp /tmp/cloudflare-ipv4.txt /etc/smartdns/cloudflare-ipv4.txt
+
+# 更新中国域名列表
+rm -f /tmp/domains.china.smartdns.conf
 curl -sS https://ghfast.top/https://raw.githubusercontent.com/leesuncom/NetGearR7000/main/common/etc/smartdns/domain-set/domains.china.smartdns.conf -o /tmp/domains.china.smartdns.conf
 cp /tmp/domains.china.smartdns.conf /etc/smartdns/domain-set/domains.china.smartdns.conf
 
-# Update GFW List
+# 更新GFW代理域名列表
+rm -f /tmp/proxy-domain-list.conf
 curl -sS https://ghfast.top/https://raw.githubusercontent.com/leesuncom/NetGearR7000/main/common/etc/smartdns/domain-set/proxy-domain-list.conf -o /tmp/proxy-domain-list.conf
 cp /tmp/proxy-domain-list.conf /etc/smartdns/domain-set/proxy-domain-list.conf
 
-# Update address.conf
+# 更新广告过滤规则
+# rm -f /tmp/address.conf
 # curl -sS https://ghfast.top/https://raw.githubusercontent.com/Cats-Team/AdRules/main/smart-dns.conf -o /tmp/address.conf
 # cp /tmp/address.conf /etc/smartdns/address.conf
 
-# onf-file /etc/smartdns/anti-ad-smartdns.conf
+# 更新反广告配置
+# rm -f /tmp/anti-ad-smartdns.conf
 # curl -sS https://anti-ad.net/anti-ad-for-smartdns.conf -o /tmp/anti-ad-smartdns.conf
 # cp /tmp/anti-ad-smartdns.conf /etc/smartdns/conf.d/anti-ad-smartdns.conf
 
-
-/etc/init.d/smartdns restart
-
+if /etc/init.d/smartdns restart; then
+  echo "SmartDNS 重启成功"
+else
+  echo "Error: SmartDNS 重启失败，请检查配置文件"
+  exit 1
+fi
 
