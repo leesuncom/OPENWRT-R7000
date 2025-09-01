8e2fba7b4b1498cfe9537c4d7a310a4bdaf185e5
leesuncom <1925600159@qq.com>
2025-09-01 13:26:06 +0800

    Update r7000lede-part2.sh

--- r7000lede-part2.sh
+++ r7000lede-part2.sh
@@ -18,23 +18,11 @@
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名
 sed -i 's/ImmortalWrt/R7000/g' package/base-files/files/bin/config_generate
 
-rm -rf target/linux/generic/hack-6.6/767-net-phy-realtek-add-led*
-wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
-
-# 移除procd-ujail
-sed -i "s/procd-ujail//" include/target.mk
-
-# 修改终端启动行为
-sed -i 's/max_requests 3/max_requests 20/g' package/network/services/uhttpd/files/uhttpd.config
-sed -i "s/tty\(0\|1\)::askfirst/tty\1::respawn/g" target/linux/*/base-files/etc/inittab
-
-# 移除jool包
-rm -rf package/feeds/packages/jool
 
 # 下载默认设置文件（已注释，如需启用请移除#）
 # wget -N https://raw.githubusercontent.com/leesuncom/NetGearR7000/refs/heads/main/default-settings/files/99-default-settings-chinese \
