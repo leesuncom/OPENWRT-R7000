05490ee703dee42b7c6dcf4e2f2302b98f2df614
leesuncom <1925600159@qq.com>
2025-09-01 20:05:07 +0800

    9.1-20

--- devices/common/diy/package/base-files/files/etc/hosts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# 删除
-sudo sed -i '/# ING Hosts Start/,/# ING Hosts End/d' /etc/hosts
-# 添加
-curl -s -k -L https://ghfast.top/https://raw.githubusercontent.com/shidahuilang/hosts/main/hosts | sudo tee -a /etc/hosts
-
-curl -sS https://www.cloudflare.com/ips-v4/ > /etc/smartdns/ip-set/cloudflare-ipv4.txt
-
-/etc/init.d/smartdns restart
